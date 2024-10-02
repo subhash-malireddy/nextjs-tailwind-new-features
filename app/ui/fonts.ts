dd580d34c587ad259139af5204bef263124d77d4
SubashMallireddy <subash@tonic.com.au>
2024-10-02 15:56:37 +1000

    chap3.1 fonts - using next/font to load some google fonts. It's possible to load custom fonts too with some limitations

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Edu_VIC_WA_NT_Beginner, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const noNswNovice = Edu_VIC_WA_NT_Beginner({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
