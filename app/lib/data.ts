9156583c5a74f7bb5fc2b46cc838ce672334c5c7
SubashMallireddy <subash@tonic.com.au>
2024-10-12 19:41:16 +1100

    chap:12 Mutating Data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,7 +30,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
