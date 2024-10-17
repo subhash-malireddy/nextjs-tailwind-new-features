00b2ee4ce122c0e9dc8134f9ab5767d659a262d2
SubashMallireddy <subash@tonic.com.au>
2024-10-17 11:41:56 +1100

    chap:15 adding authentication for the app using nextauth 5 beta

---
+++ auth.ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import { User } from "./app/lib/definitions";
+import { sql } from "@vercel/postgres";
+
+import bcrypt from "bcrypt";
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (!parsedCredentials.success) return null;
+
+        const { email, password } = parsedCredentials.data;
+
+        const user = await getUser(email);
+
+        if (!user) return null;
+
+        const pwdMatch = bcrypt.compareSync(password, user.password);
+
+        if (!pwdMatch) return null;
+
+        console.log("Invalid credentials");
+        return user;
+      },
+    }),
+  ],
+});
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
