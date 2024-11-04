c065034731bc0d90c2d22bc6705d0f70c2dac0c7
SubashMallireddy <subash@tonic.com.au>
2024-11-04 22:51:46 +1100

    sync system theme with the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.scss";
 import { noNswNovice } from "./ui/fonts";
-import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-skin-fill-accent-base p-4 md:h-52">
+    <main className="flex min-h-screen flex-col">
+      <div className="flex h-20 shrink-0 items-end bg-skin-fill-accent-base p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col justify-center gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-skin-fill-secondary-default px-6 py-10 md:px-20">
+        <div className="flex flex-col justify-center gap-6 bg-skin-fill-secondary-default px-6 py-10 md:px-20">
           <div className="relative h-0 w-0 border-b-[26px] border-l-[15px] border-r-[15px] border-b-[var(--color-text-base)] border-l-transparent border-r-transparent" />
           <p
             className={`text-xl text-skin-base md:text-3xl md:leading-normal ${noNswNovice.className}`}
