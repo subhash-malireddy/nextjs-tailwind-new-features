303bf283f8d5bf0946ac1e2f4751fc9ad3c6a5c7
SubashMallireddy <subash@tonic.com.au>
2024-11-14 11:02:54 +1100

    implement toggling light and dark theme

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,20 +1,23 @@
 import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
-import { PowerIcon } from "@heroicons/react/24/outline";
+import { PowerIcon, SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 import { signOut } from "@/auth";
+import ThemeButton from "./theme-button";
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="bg-skin-fill-accent-base mb-2 flex h-20 items-end justify-start rounded-md p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-skin-fill-accent-base p-4 md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
         </div>
       </Link>
+      {/* theme toggling button */}
+      <ThemeButton />
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-skin-fill-secondary-default md:block"></div>
