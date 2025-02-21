d8ff2c388c81cfab62ce3c23e7c867bdbe61f37b
SubashMallireddy <subash@tonic.com.au>
2025-02-22 09:54:57 +1100

    feat: add ability to toggle dark/light themes with next-themes package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
@@ -7,8 +8,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider enableSystem defaultTheme="system">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/ui/dashboard/theme-button.tsx
+++ app/ui/dashboard/theme-button.tsx
@@ -1,45 +1,28 @@
 "use client";
 
-import useSystemTheme from "@/app/hooks/useTheme";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
-
+import { useTheme } from "next-themes";
 export default function ThemeButton() {
-  const theme = useSystemTheme();
-  console.log("🚀 ~ ThemeButton ~ theme:", theme);
-  const [isDarkMode, setIsDarkMode] = useState(() =>
-    theme === "dark" ? true : false,
-  );
+  const { theme, setTheme } = useTheme();
+  const isDarkMode = theme === "dark" ? true : false;
 
   const toggleTheme = () => {
-    // const currentTheme = !isDarkMode;
-    setIsDarkMode(!isDarkMode);
-    // Add your code to switch between light and dark themes here.
-
-    const body = document.body;
-    // if the current theme is not dark
-    if (!isDarkMode) {
-      body.classList.remove("theme-light");
-      body.classList.add("theme-dark");
+    if (isDarkMode) {
+      setTheme("light");
     } else {
-      // if the current theme is not light
-      body.classList.remove("theme-dark");
-      body.classList.add("theme-light");
+      setTheme("dark");
     }
   };
 
   return (
     <button
-      //   className={`rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-offset-2 ${
-      //     isDarkMode ? "bg-gray-800 text-white" : "bg-white text-gray-800"
-      //   }`}
       className="mb-2 flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-skin-fill-secondary-default p-3 text-sm font-medium hover:bg-skin-fill-secondary-active hover:text-skin-active md:flex-none md:justify-start md:p-2 md:px-3"
       onClick={toggleTheme}
     >
       {isDarkMode ? (
-        <MoonIcon className="h-5 w-5" />
-      ) : (
         <SunIcon className="h-5 w-5" />
+      ) : (
+        <MoonIcon className="h-5 w-5" />
       )}
     </button>
   );
